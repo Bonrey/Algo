bb777465ed6862bbf88183136b54d897f1b25b7a
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-05-16 08:50:00 +0300

    Create selectionSort.js

---
+++ algo/selectionSort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+
+    [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+}
+
+
+let a = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+selectionSort(a);
+
+console.log(a);
