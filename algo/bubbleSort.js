614a4b35d9f202aedfe9de683f29ad47b418ad70
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-05-16 08:44:29 +0300

    Create bubbleSort.js

---
+++ algo/bubbleSort.js
@@ -0,0 +1,22 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let swapped = false;
+    
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        swapped = true;
+      }
+    }
+
+    if (!swapped) {
+      break;
+    }
+  }
+}
+
+
+let a = [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92];
+bubbleSort(a);
+
+console.log(a);
