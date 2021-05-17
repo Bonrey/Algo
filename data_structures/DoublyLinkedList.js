339a1c1b9853d313b50367d10c9f2b6b2a521c39
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-05-17 20:42:08 +0300

    Update DoublyLinkedList.js

--- data_structures/DoublyLinkedList.js
+++ data_structures/DoublyLinkedList.js
@@ -5,13 +5,14 @@ const Node = function (data, prev) {
 };
 
 const DoublyLinkedList = function () {
+  let length = 0;
   this.head = null;
   this.tail = null;
 
   this.add = function (el) {
     const newNode = new Node(el, this.tail);
 
-    if (!this.head) {
+    if (this.size() === 0) {
       this.head = newNode;
       this.tail = newNode;
     } else {
@@ -20,18 +21,22 @@ const DoublyLinkedList = function () {
       this.tail = this.tail.next;
       this.tail.prev = tmp;
     }
-  }
+
+    length++;
+  };
 
   this.remove = function (el) {
-    if (!this.head) return null;
+    if (this.size() === 0) return null;
 
     while (this.head && this.head.data === el) {
       this.head = this.head.next;
+      length--;
     }
 
     let currentNode = this.head;
     while (currentNode) {
       if (currentNode.data === el) {
+        length--;
         currentNode.prev.next = currentNode.next;
         if (currentNode.next) {
           currentNode.next.prev = currentNode.prev;
@@ -39,6 +44,36 @@ const DoublyLinkedList = function () {
       }
       currentNode = currentNode.next;
     }
-  }
-};
+  };
 
+  this.size = function() {
+    return length;
+  };
+
+  this.print = function() {
+    let currentNode = this.head;
+    let result = [];
+
+    while (currentNode) {
+      result.push(currentNode.data);
+      currentNode = currentNode.next;
+    }
+
+    console.log(result.join(', '));
+  };
+
+  this.reverse = function () {
+    if (this.size() === 0) return null;
+
+    this.tail = this.head;
+    let currentNode = this.head;
+
+    while (currentNode.next) {
+      [currentNode.next, currentNode.prev] = [currentNode.prev, currentNode.next];
+      currentNode = currentNode.prev;
+    }
+
+    this.head = currentNode;
+    [this.head.prev, this.head.next] = [null, currentNode.prev];
+  };
+};
