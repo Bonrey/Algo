d81409e4b7d21b047371c598c82326331fe36197
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-05-23 18:59:14 +0300

    Create partitions.js

---
+++ miscellaneous/partitions.js
@@ -0,0 +1,20 @@
+function partitions(num) {
+  const result = [];
+
+  function solve(n, curr) {
+    if (n === 0) {
+      result.push(curr.join('+'));
+      return;
+    }
+
+    for (let i = 1; i <= n; i++) {
+      solve(n - i, [...curr, i]);
+    }
+  }
+
+  solve(num, []);
+  return result;
+}
+
+// Example
+numberOfNames(5).forEach(expr => console.log(expr));
