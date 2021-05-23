eb0787ab53707e9e22726cf506f25fbb3d1750ed
Bonrey <51796309+Bonrey@users.noreply.github.com>
2021-05-23 14:47:51 +0300

    Create MaxHeap.js

---
+++ data_structures/MaxHeap.js
@@ -0,0 +1,57 @@
+const MaxHeap = function () {
+  const heap = [null];
+
+  this.insert = function (node) {
+    heap.push(node);
+    let i = heap.length - 1;
+
+    while (heap[Math.floor(i / 2)] && heap[i] > heap[Math.floor(i / 2)]) {
+      [heap[i], heap[Math.floor(i / 2)]] = [heap[Math.floor(i / 2)], heap[i]];
+      i = Math.floor(i / 2);
+    }
+  }
+
+  this.remove = function () {
+    if (!this.size()) return null;
+
+    const max = heap[1];
+    const last = heap.pop();
+
+    let i = 1;
+    heap[i] = last;
+    while (heap[i]) {
+      let left = heap[i * 2];
+      let right = heap[i * 2 + 1];
+      if (!left && !right) break;
+
+      let j = i * 2;
+      if (left && right) {
+        j += (left < right);
+      } else if (right) {
+        j++;
+      }
+
+      [heap[i], heap[j]] = [heap[j], heap[i]];
+      i = j;
+    }
+
+    return max;
+  }
+
+  this.size = function () {
+    return heap.length - 1;
+  }
+
+  this.print = function () {
+    return heap.slice(1);
+  }
+};
+
+
+let heap = new MaxHeap();
+heap.insert(3);
+heap.insert(2);
+heap.insert(9);
+heap.insert(7);
+console.log(heap.remove());
+console.log(heap.print());
